e33d638b2966a2ecfd0c2d586818706c21e83aaa
Johannes Maron <johannes@maron.family>
2023-12-22 12:00:16 +0100

    Support nested node_modules

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
+import pytest
+
 from django_esm import utils
 
+FIXTURE_DIR = Path(__file__).parent
 
-def test_parse_package_json(package_json):
-    import_map = dict(utils.parse_package_json(package_json.parent))
+
+def test_parse_root_package(package_json):
+    import_map = dict(utils.parse_root_package(package_json))
     assert import_map["htmx.org"] == "/static/htmx.org/dist/htmx.min.js"
     assert import_map["lit"] == "/static/lit/index.js"
     assert (
@@ -10,3 +16,37 @@ def test_parse_package_json(package_json):
         == "/static/%40lit/reactive-element/reactive-element.js"
     )
     assert import_map["lit-html"] == "/static/lit-html/lit-html.js"
+    assert import_map["#index"] == "/static/js/index.js"
+    assert import_map["#components/"] == "/static/js/components/"
+    assert import_map["#htmx"] == "https://unpkg.com/htmx.org@1.9.10"
+
+
+def test_parse_root_package__bad_imports(package_json):
+    package_json["imports"] = "foo"
+    with pytest.raises(ValueError) as e:
+        dict(utils.parse_root_package(package_json))
+    assert "must be an object" in str(e.value)
+
+    package_json["imports"] = ["foo"]
+    with pytest.raises(ValueError) as e:
+        dict(utils.parse_root_package(package_json))
+    assert "must be an object" in str(e.value)
+
+
+def test_parse_root_package__bad_keys(package_json):
+    package_json["imports"] = {"foo": "/bar"}
+    with pytest.raises(ValueError) as e:
+        dict(utils.parse_root_package(package_json))
+    assert "must start with #" in str(e.value)
+
+
+def test_cast_exports():
+    assert utils.cast_exports({"exports": {"foo": "bar"}}) == {"foo": "bar"}
+    assert utils.cast_exports({"exports": "foo"}) == {".": "foo"}
+    assert utils.cast_exports({"exports": ["foo"]}) == {"foo": "foo"}
+
+
+def test_get_static_from_abs_path():
+    with pytest.raises(ValueError) as e:
+        utils.get_static_from_abs_path(Path("/foo/bar"))
+    assert "Could not find" in str(e.value)
--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -76,6 +76,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "node_modules",
 ]
