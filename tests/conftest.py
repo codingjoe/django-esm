bbadefcc7b5d117680a9a8c6f9f711e905caec8f
Johannes Maron <johannes@maron.family>
2023-12-22 22:51:49 +0100

    Revert test changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import json
-import os
 import subprocess
-import sys
 from pathlib import Path
 
 import pytest
@@ -12,11 +10,6 @@ TEST_DIR = Path(__file__).parent
 @pytest.fixture(scope="session")
 def package_json():
     subprocess.check_call(["npm", "install", "--omit=dev"], cwd=TEST_DIR)
-    subprocess.check_call(
-        [sys.executable, "manage.py", "collectstatic", "--noinput"],
-        cwd=TEST_DIR,
-        env={"PYTHONPATH": ".."} | os.environ,
-    )
     with (TEST_DIR / "package.json").open() as f:
         return json.load(f)
 
